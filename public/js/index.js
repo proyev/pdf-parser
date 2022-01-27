4c552095f8881b77b4c345cc2382f07a8dd5c1e0
Yevhen Prots <prots.yevhen@gmail.com>
2022-01-27 03:11:23 +0100

    fix: functional input file replication

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+   const uploadButton = document.getElementById('import-pdf-button');
+   const chooseDocument = document.getElementById('import-pdf');
+
+   uploadButton.addEventListener('click', (event) => chooseDocument.click());
 
 });
